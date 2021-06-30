8c6ff0cd23b4f52bb0800ffc60c6e39d5fc78eb6
Pavel Sadchenko <psadchenko@tango.me>
2021-06-30 21:50:02 +0300

    Generated l10n messages for rocketmailforward module

---
+++ rocketmailforward/messages/ko/base.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    '(Rocket) <strong>Mail forward</strong>' => '',
+    '<strong>Rocket Mail Forward</strong> module configuration' => '',
+    'Choose user' => '',
+    'Create new forwarding rule' => '',
+    'Endpoint' => '',
+    'Forwarding web address for post messages' => '',
+    'Http endpoint' => '',
+    'Incoming messages of the following users are being forwarded.' => '',
+    'Mail messages forward' => '',
+    'Maximum waiting time for sending http request with new message' => '',
+    'Pick the recipient for whom inbox messages will be forwarded' => '',
+    'Recipient' => '',
+    'Rule deleted' => '',
+    'The specified user has forwarding rule already set.' => '',
+    'Update forwarding rule for the user #{user_id}' => '',
+];
